4b86f01474a143964a610984ea9c7d5bf33b9a0b
Swastik WhitehatJR <81065922+Swastik-WhitehatJR@users.noreply.github.com>
2022-12-21 21:54:22 +0530

    Add files via upload

---
+++ PROJECT138/app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+df_articles = pd.read_csv("shared_articles.csv")
+df_users = pd.read_csv("users_interactions.csv")
+
+print(df_articles.head())
+printdf_users .head())
